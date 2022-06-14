79cd7f131501bbf335da3a17a7537ea9a4274a89
sangah <sangah0222@naver.com>
2022-06-14 14:27:18 +0900

    Add result

--- drawing-lots/src/Choice/Choice.jsx
+++ drawing-lots/src/Choice/Choice.jsx
@@ -6,11 +6,17 @@ import "./Choice.css";
 const Choice = () => {
   const [isOpen, setIsOpen] = useState(false);
   const count = Number(localStorage.getItem("count"));
-  const array = Array.from({ length: count }, () => 0);
+  const array = localStorage.getItem("array");
+  const items = array.split(",");
   const navigate = useNavigate();
 
   const openModal = () => {
     setIsOpen(true);
+  };
+
+  const getResult = () => {
+    const randNum = Math.floor(Math.random() * (count - 1 + 1)) + 1;
+    return randNum - 1;
   };
 
   const goToNext = () => {
@@ -18,11 +24,18 @@ const Choice = () => {
     navigate("/");
   };
 
+  const retry = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="main">
-      <div className="title">쪽지를 골라주세요</div>
+      <div className="title">
+        쪽지를 골라주세요
+        <div className="info">쪽지의 순서는 결과와 상관이 없습니다</div>
+      </div>
       <div className="items">
-        {array.map(() => (
+        {items.map(() => (
           <div className="item" onClick={openModal}>
             <img src={Item} />
           </div>
@@ -31,9 +44,12 @@ const Choice = () => {
       <div className={isOpen ? "background show" : "background"}>
         <div className="window">
           <div className="popup">
-            <div className="text">hi</div>
-            <div className="nextButton" onClick={goToNext}>
-              처음으로
+            <div className="text">{items[getResult()]}</div>
+            <div className="buttons">
+              <div className="retryButton" onClick={retry}>
+                다시하기
+              </div>
+              <div onClick={goToNext}>처음으로</div>
             </div>
           </div>
         </div>
